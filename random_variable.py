58a24ef524bb4fcf46b69b056d7e99a3f66e76c0
NearIsCoding <conansebastian@gmail.com>
2022-08-17 21:12:52 -0500

    Modificacion del metodo generador

--- random_variable.py
+++ random_variable.py
@@ -3,13 +3,14 @@ from math import log
 
 def generate(quantity):
     seed = random()
-    lambdaNumber = 0.1
+    lambdaNumber = 0.429
     generatedNumbers = []
     for i in range(quantity):
         seed = fixSeed(seed)
         print(type(seed))
         print(type(lambdaNumber))
         seed = ((-(log(1 - seed)))/lambdaNumber)
+        seed= fixSeed(seed)
         generatedNumbers.append(seed)
     return generatedNumbers
     
